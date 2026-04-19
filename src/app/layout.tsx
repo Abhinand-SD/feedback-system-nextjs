275ea0566a50d250ea6af493548374c00c96568b
Abhinand SD <abhinandssd90@gmail.com>
2026-04-20 00:45:07 +0530

    update title to ASD Hospital

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import { Toaster } from 'react-hot-toast';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Feedback System',
+  title: 'ASD Hospital',
   description: 'Share your thoughts and feedback',
 };
 
